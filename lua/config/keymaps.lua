aeae31a6805b8d2d9d7a2f62bb11d7d8d297a139
vetsasp <vetsasp@gmail.com>
2025-11-30 01:42:28 -0500

    Organized keymaps by function. Added some keymaps regarding the quickfix list.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,11 @@
 local map = vim.keymap.set
 
+----- ESSENTIALS -----
 map('n', ';', ':', { desc = 'CMD enter command mode' })
-
 -- Better escape from insert and visual mode
 map({ 'i', 'v' }, 'jk', '<ESC>')
 
+----- NAVIGATION -----
 -- Start and End of lines
 map({ 'n', 'v' }, '<S-H>', '^', { desc = 'Move to first non-space character' })
 map({ 'n', 'v' }, '<S-L>', 'g_', { desc = 'Move to last non-space character' })
@@ -24,6 +25,7 @@ map('n', '<C-u>', '<C-u>zz')
 map('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next buffer' })
 map('n', '<S-Tab>', '<cmd>bprev<CR>', { desc = 'Previous buffer' })
 
+----- IMPROVEMENTS -----
 -- Searches centered
 map('n', 'n', 'nzzzv')
 map('n', 'N', 'Nzzzv')
@@ -31,16 +33,8 @@ map('n', 'N', 'Nzzzv')
 -- paste without yanking
 map('x', '<leader>p', '"_dP')
 
--- Toggle line comment
-map({ 'n', 'v' }, '<leader>/', function()
-  vim.cmd 'normal gcc'
-end, { desc = 'Toggle comment' })
-
 -- <leader>l to open Lazy
 map('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Lazy' })
-
--- open floating terminal
--- map({ 'n', 't' }, '<leader>ft', '<cmd>Floaterminal<CR>', { desc = 'Toggle floating terminal' })
 
 -- copy all
 map('n', '<C-c>', 'gg"+yG', { desc = 'Copy [A]ll to system clipboard' })
@@ -48,11 +42,29 @@ map('n', '<C-c>', 'gg"+yG', { desc = 'Copy [A]ll to system clipboard' })
 -- close current buffer
 map('n', '<leader>x', '<cmd>bd<CR>', { desc = 'Close current buffer' })
 
--- Diagnostic keymaps
-map('n', '<leader>qq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Toggle line comment
+map({ 'n', 'v' }, '<leader>/', function()
+  vim.cmd 'normal gcc'
+end, { desc = 'Toggle comment' })
+
+----- QUICKFIX -----
+map('n', '<leader>qq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>qn', '<cmd>cnext<CR>', { desc = 'Go to [N]ext diagnostic in buffer' })
+map('n', '<leader>qp', '<cmd>cprev<CR>', { desc = 'Go to [P]revious diagnostic in buffer' })
+map('n', '<leader>qt', function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+    if ft == 'qf' then
+      vim.cmd 'cclose'
+      return
+    end
+  end
+  vim.cmd 'copen'
+end, { desc = '[T]oggle quickfix list' })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
